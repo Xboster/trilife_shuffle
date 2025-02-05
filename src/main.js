60d3e6f04a25e9d132d8b1d50b27f335f3186515
Leon <lng103@ucsc.edu>
2025-02-05 13:43:23 -0800

    added menuScene

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// Leon Ng
+// Trilife Shuffle
 let config = {
     type: Phaser.AUTO,
     width: 480,
@@ -6,3 +8,6 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
---
+++ src/scenes/Menu.js
@@ -0,0 +1,47 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene");
+    }
+    init() {}
+    preload() {}
+    create() {
+        let menuConfig = {
+            fontFamily: "Courier",
+            fontSize: "28px",
+            backgroundColor: "#F3B141",
+            color: "#843605",
+            align: "right",
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0,
+        };
+
+        menuConfig.backgroundColor = "#00FF00";
+        menuConfig.color = "#000";
+        this.add
+            .text(
+                game.config.width / 2,
+                game.config.height / 2 - borderUISize - borderPadding,
+                "Trilife Shuffle",
+                menuConfig
+            )
+            .setOrigin(0.5);
+
+        this.add
+            .text(
+                game.config.width / 2,
+                (game.config.height / 4) * 3 - borderUISize - borderPadding,
+                "Press any key to start",
+                menuConfig
+            )
+            .setOrigin(0.5);
+    }
+    update() {
+        this.input.keyboard.on("keydown", () => {
+            this.scene.start("playScene");
+            console.log("PLAY SCENE");
+        });
+    }
+}
